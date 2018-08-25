cca4800daa50c78add1b3f7cb93bd9b730ded274
alexpryp <alexpryp@gmail.com>
2018-08-25 17:21:39 +0300

    changed navigation bar and main chapter

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict"
+
+//script that simulates active tabs
+function activateTab(evt) {
+// Declare all variables
+    var i, tablinks;
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    //Add an "active" class to the button that opened the tab
+    evt.currentTarget.className += " active";
+}
