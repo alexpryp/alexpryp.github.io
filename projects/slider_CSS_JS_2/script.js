e8c03253ebf0601e251b807cea82ce0927f7874f
alexpryp <alexpryp@gmail.com>
2019-05-31 10:42:07 +0300

    Project Slider has been updated

--- projects/slider_CSS_JS_2/script.js
+++ projects/slider_CSS_JS_2/script.js
@@ -1,36 +1,88 @@
 "use strict"
 
 let lis = document.getElementsByTagName("li");
-
 let width = 1000;
-
 let slider = document.querySelector(".slider");
 let list = slider.querySelector("ul");
 let listElems = slider.querySelectorAll("li");
-
+let timerId = null;
+let thumbs = document.querySelector(".slider-thumbs");
+let selectThumb = document.querySelector("img[data-number='1']");
 let position = 0;
 
+let slides = {
+	"1": 0,
+	"2": 1000,
+	"3": 2000,
+	"4": 3000,
+	"0": 1,
+	"1000": 2,
+	"2000": 3,
+	"3000": 4
+}
+
+//adding a thumbSelect class to the corresponding thumbnail
+function highlight(position) {
+	let elem = null;
+	let slideNum = slides[ ""+ Math.abs(position)];
+	
+	elem = `img[data-number="${slideNum}"]`;
+	elem = document.querySelector(elem);
+
+	if(selectThumb) {
+		selectThumb.classList.remove("thumbSelect");
+	}
+
+	selectThumb = elem;
+	elem.classList.add("thumbSelect")
+}
+
+//switch slides to the left
 function back () {
 	position = Math.min(position + width, 0);
+	highlight(position);
 	list.style.marginLeft = position + "px";
 }
 
+//switch slides to the right
 function forward () {
 	position = Math.max(position - width, -width * (listElems.length - 1));
+	highlight(position);
 	list.style.marginLeft = position + "px";	
 }
 
-slider.querySelector('.prev').onclick = back;
+slider.querySelector('.prev').addEventListener("click", back);
+slider.querySelector('.next').addEventListener("click", forward);
 
-slider.querySelector('.next').onclick = forward;
-
+//automatic scrolling through slides
 function autoSlide () {
 	if (position > -3000) {
 		forward();
 	} else {
 		position = 0;
+		highlight(0);
 		list.style.marginLeft = "0px";
 	}
 }
 
-let timerId = setInterval(autoSlide, 5000);
+timerId = setInterval(autoSlide, 5000);
+
+//slide show that matches the selected thumbnail
+function showLargeImg(event) {
+	let target = event.target;
+
+	while (target != this) {
+		if(target.nodeName == "IMG") {
+			if(selectThumb) {
+				selectThumb.classList.remove("thumbSelect");
+			}
+			
+			selectThumb = target;
+			target.classList.add("thumbSelect");
+			list.style.marginLeft = -slides[target.getAttribute('data-number')] + "px";
+		}
+		target = target.parentNode;
+	}
+}
+
+thumbs.addEventListener("click", showLargeImg);
