6755bb780e399741c289fbf302ebd6b555758fa3
alexpryp <alexpryp@gmail.com>
2020-01-01 03:44:43 +0200

    Tictactoe_game_react project was added

---
+++ projects/tictactoe_game_react/precache-manifest.7b26749f5fe26d47d50665848956e1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6979e8b8da451c4d2d61beea642c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37dfc3a9cd4671fcdfa4",
+    "url": "/static/css/main.9c473dc5.chunk.css"
+  },
+  {
+    "revision": "8096ce7179baedc7d19e",
+    "url": "/static/js/2.cf18ddba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cf18ddba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37dfc3a9cd4671fcdfa4",
+    "url": "/static/js/main.7859f1d2.chunk.js"
+  },
+  {
+    "revision": "6843748bb8dd06c04883",
+    "url": "/static/js/runtime-main.40b62207.js"
+  }
+]);
