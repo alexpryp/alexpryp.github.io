4cba14bb686199e01ee672948af9b3ca23005fdf
alexpryp <alexpryp@gmail.com>
2020-01-12 09:22:07 +0200

    Added project Calculator_React

---
+++ projects/calculator_react/precache-manifest.fcffa4a75a81de9da0c265fff783b35f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec596fdfb34aea281162ba8735718ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c919afe3ded58bef2fb",
+    "url": "/static/css/main.74ad0e91.chunk.css"
+  },
+  {
+    "revision": "6de130a4b0e9651a0edf",
+    "url": "/static/js/2.1c1b259a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1c1b259a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c919afe3ded58bef2fb",
+    "url": "/static/js/main.32746dae.chunk.js"
+  },
+  {
+    "revision": "ae0331b34ba35ef4c1d0",
+    "url": "/static/js/runtime-main.f5759406.js"
+  }
+]);
