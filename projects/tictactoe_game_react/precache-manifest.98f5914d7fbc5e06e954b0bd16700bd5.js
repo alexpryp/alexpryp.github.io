09dd40df135c3b06f7b66444fd38cdc06e709ab5
alexpryp <alexpryp@gmail.com>
2020-01-23 23:11:59 +0200

    Added game modes to the tictactoe_game_react project

---
+++ projects/tictactoe_game_react/precache-manifest.98f5914d7fbc5e06e954b0bd16700bd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a529895f7ab0bdcfaa5295999f7322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25557f9e4c39c9ecbd77",
+    "url": "/static/css/main.a777785c.chunk.css"
+  },
+  {
+    "revision": "6deec5a63d2659a0da92",
+    "url": "/static/js/2.d5598dfa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d5598dfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25557f9e4c39c9ecbd77",
+    "url": "/static/js/main.9aa3ffdd.chunk.js"
+  },
+  {
+    "revision": "6843748bb8dd06c04883",
+    "url": "/static/js/runtime-main.40b62207.js"
+  }
+]);
