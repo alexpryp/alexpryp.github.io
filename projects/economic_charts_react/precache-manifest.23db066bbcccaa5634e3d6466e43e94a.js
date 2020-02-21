9d0c8e176bc6624ab78e723ec663860474e23259
alexpryp <alexpryp@gmail.com>
2020-02-21 03:05:21 +0200

    Added economic_charts_react project

---
+++ projects/economic_charts_react/precache-manifest.23db066bbcccaa5634e3d6466e43e94a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ec75f248a48e2c7404b23dafc0dfa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd5d8110f1905a7c9b5d",
+    "url": "/static/css/main.97adf01e.chunk.css"
+  },
+  {
+    "revision": "cb5a9f59d2823df7b301",
+    "url": "/static/js/3.05d3a2b8.chunk.js"
+  },
+  {
+    "revision": "0e5076192344aa73beb2",
+    "url": "/static/js/4.ebe16f77.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/4.ebe16f77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2609a362fba75dfb263",
+    "url": "/static/js/5.3c487c4d.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.3c487c4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3adad203b20207731f54",
+    "url": "/static/js/6.64c0054c.chunk.js"
+  },
+  {
+    "revision": "1f511d5d7baea7a663db7666f6bcaff2",
+    "url": "/static/js/6.64c0054c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd5d8110f1905a7c9b5d",
+    "url": "/static/js/main.f1d7afef.chunk.js"
+  },
+  {
+    "revision": "6ea1732a4d2d9f7b62ab",
+    "url": "/static/js/runtime-main.802e701e.js"
+  },
+  {
+    "revision": "f63b3c9ba12ba91f2c2f",
+    "url": "/static/js/xlsx.bffacbfc.chunk.js"
+  }
+]);
---
+++ projects/economic_charts_react/static/js/xlsx.bffacbfc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpeconomic_charts_react=this.webpackJsonpeconomic_charts_react||[]).push([[2],{239:function(c,n){},325:function(c,n){},326:function(c,n){}}]);
+//# sourceMappingURL=xlsx.bffacbfc.chunk.js.map
