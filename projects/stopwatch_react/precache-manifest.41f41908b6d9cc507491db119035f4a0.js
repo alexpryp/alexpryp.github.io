b5ac7ead4c8fa14d5de9516343190d484abfa443
alexpryp <alexpryp@gmail.com>
2020-07-10 19:52:00 +0300

    Added Stopwatch_react project

---
+++ projects/stopwatch_react/precache-manifest.41f41908b6d9cc507491db119035f4a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c5585fc981e21c5a11b4a4917cb54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0773ffe4bcb74aebd710",
+    "url": "/static/css/main.321175e6.chunk.css"
+  },
+  {
+    "revision": "e60553b31f4283ddbb4d",
+    "url": "/static/js/2.996b2dc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.996b2dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0773ffe4bcb74aebd710",
+    "url": "/static/js/main.03e5683d.chunk.js"
+  },
+  {
+    "revision": "5bac125d76ffb34ed028",
+    "url": "/static/js/runtime-main.2a392178.js"
+  }
+]);
