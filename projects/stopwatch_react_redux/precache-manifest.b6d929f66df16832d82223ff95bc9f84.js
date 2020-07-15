2e6c74114b0881afeafa3476855aadc6bf8c7f55
alexpryp <alexpryp@gmail.com>
2020-07-15 15:52:59 +0300

    Added Stopwatch project with React and Redux

---
+++ projects/stopwatch_react_redux/precache-manifest.b6d929f66df16832d82223ff95bc9f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452a7c68f29707662887069d9abbd351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474aa5811d07f628fafc",
+    "url": "/static/css/main.07175252.chunk.css"
+  },
+  {
+    "revision": "e624ac16492f86242c3b",
+    "url": "/static/js/2.b57fcafb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b57fcafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474aa5811d07f628fafc",
+    "url": "/static/js/main.0ec2a5ff.chunk.js"
+  },
+  {
+    "revision": "11968cdd7c9a71f1e5d4",
+    "url": "/static/js/runtime-main.bfe68fa7.js"
+  }
+]);
